b8c6346915eafd37a5d482ad92b31e43b5a60734
Brian Chiruka <attomg@gmail.com>
2021-10-29 06:41:37 +0200

    🔥memory-server: fix issue

--- app.js
+++ app.js
@@ -2,31 +2,38 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 const orderbookRouter = require("./api/routes/orderbook.routes");
 
 /**
  * Connect to the in-memory database. CONFIRM CONFIRM CONFIRM CONFIRM
  */
 
-// const { MongoMemoryServer } = require("mongodb-memory-server");
-// let mongo = MongoMemoryServer;
+const connectDb = async () => {
+  const mongoServer = await MongoMemoryServer.create();
 
-// module.exports.connect = async () => {
-//   mongo = await MongoMemoryServer.create();
-//   const uri = mongo.getUri();
+  const mongoUri = mongoServer.getUri();
 
-//   const mongooseOpts = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   };
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
 
-//   await mongoose.connect(uri, mongooseOpts);
-// };
+  mongoose.connect(mongoUri, mongooseOpts);
 
-mongoose.connect(process.env.ORDERBOOK_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to orderbook"));
+  mongoose.connection.on("error", (err) => {
+    if (err.message.code === "ETIMEDOUT") {
+      throw err;
+      mongoose.connect(mongoUri, mongooseOpts);
+    }
+    throw err;
+  });
+  mongoose.connection.once("open", () => {
+    console.log(`MongoDB successfully connected to ${mongoUri}`);
+  });
+};
+
+connectDb();
 
 app.use(express.json());
 
